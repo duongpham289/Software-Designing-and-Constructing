c5ddd01a1f453a0339943f4032d18cd6283619e1
n.hieu_11 <hieubuimedia@gmail.com>
2020-05-14 17:40:09 +0700

    chỉnh đường dẫn about

--- Hotel-Suggestion/routes/web.php
+++ Hotel-Suggestion/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Client'], function () {
+    Route::get('about', 'HomeController@about');
+});
