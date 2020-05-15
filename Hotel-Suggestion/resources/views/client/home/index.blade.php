58cf1b598d80a85947fdca92379c406a70489958
duongpham289 <duongshin2000@gmail.com>
2020-05-15 07:47:48 +0700

    blade template

--- Hotel-Suggestion/resources/views/client/home/index.blade.php
+++ Hotel-Suggestion/resources/views/client/home/index.blade.php
@@ -1,3 +1,5 @@
+@extends('client.layouts.web', ['title' => 'Homepage'])
+@section('content')
 <!-- Home -->
 
 <div class="home">
@@ -1013,3 +1015,12 @@
         </div>
     </div>
 </div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
+
+@endsection
