b64922e32747906fd627f21a1a94a16b9ad13a8e
duongpham289 <duongshin2000@gmail.com>
2020-05-15 08:42:53 +0700

    Update HomeController.php

--- Hotel-Suggestion/app/Http/Controllers/Client/HomeController.php
+++ Hotel-Suggestion/app/Http/Controllers/Client/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function about(){
-        return view('client.about');
+        return view('client.home.index');
     }
 }
