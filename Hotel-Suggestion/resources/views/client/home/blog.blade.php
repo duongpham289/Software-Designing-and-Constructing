c18ab9bb25af825a460d4eda68f52a414e9ace50
duongpham289 <duongshin2000@gmail.com>
2020-05-16 15:58:55 +0700

    Update blog.blade.php

--- Hotel-Suggestion/resources/views/client/home/blog.blade.php
+++ Hotel-Suggestion/resources/views/client/home/blog.blade.php
@@ -15,7 +15,7 @@
 	<!-- Home -->
 
 	<div class="home">
-		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="/assets/clientimages/blog_background.jpg"></div>
+		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="/assets/client/images/blog_background.jpg"></div>
 		<div class="home_content">
 			<div class="home_title">the blog</div>
 		</div>
