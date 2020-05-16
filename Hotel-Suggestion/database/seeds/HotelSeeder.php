5a56c22a552c289762ab720e9d00c021a0be03bd
tuanle510 <lethientuan8668@gmail.com>
2020-05-16 17:27:33 +0700

    fix hotel, room seeder

--- Hotel-Suggestion/database/seeds/HotelSeeder.php
+++ Hotel-Suggestion/database/seeds/HotelSeeder.php
@@ -11,14 +11,15 @@ class hotel extends Seeder
      */
     public function run()
     {
-        DB::table('hotel')->insert
-            (['name' => 'Mường thanh','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'baro','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'hello','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'Biển xanh','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'Hà Nội','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'Sao Mai','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'Đâu Cũng Được','address' => 'Hà Nội','images' =>'', 'details'=>'']);
-            (['name' => 'Sài Gòn','address' => 'Hà Nội','images' =>'', 'details'=>'']);
+        DB::table('hotel')->insert([
+            ['name' => 'Mường thanh','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'baro','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'hello','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'Biển xanh','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'Hà Nội','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'Sao Mai','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'Đâu Cũng Được','address' => 'Hà Nội','images' =>'', 'details'=>''],
+            ['name' => 'Sài Gòn','address' => 'Hà Nội','images' =>'', 'details'=>''],
+        ]);
     }
 }
--- Hotel-Suggestion/database/seeds/RoomSeeder.php
+++ Hotel-Suggestion/database/seeds/RoomSeeder.php
@@ -11,12 +11,13 @@ class RoomSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('room')->insert
-        (['name' => '102','type' => 'Phòng đôi','price' =>'200', 'details'=>'']);
-        (['name' => '101','type' => 'Phòng đơn','price' =>'250', 'details'=>'']);
-        (['name' => '106','type' => 'Phòng đôi','price' =>'170', 'details'=>'']);
-        (['name' => '205','type' => 'Phòng đơn','price' =>'350', 'details'=>'']);
-        (['name' => '207','type' => 'Phòng đôi','price' =>'260', 'details'=>'']);
-        (['name' => '301','type' => 'Phòng đơn','price' =>'180', 'details'=>'']);
+        DB::table('room')->insert([
+        ['name' => '102','type' => 'Phòng đôi','price' =>'200', 'details'=>''],
+        ['name' => '101','type' => 'Phòng đơn','price' =>'250', 'details'=>''],
+        ['name' => '106','type' => 'Phòng đôi','price' =>'170', 'details'=>''],
+        ['name' => '205','type' => 'Phòng đơn','price' =>'350', 'details'=>''],
+        ['name' => '207','type' => 'Phòng đôi','price' =>'260', 'details'=>''],
+        ['name' => '301','type' => 'Phòng đơn','price' =>'180', 'details'=>''],
+        ]);
     }
 }
