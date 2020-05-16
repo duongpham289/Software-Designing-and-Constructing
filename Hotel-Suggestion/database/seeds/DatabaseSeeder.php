be8c7820ce2c3a9da1b873146c4f44823cdd59a4
n.hieu_11 <hieubuimedia@gmail.com>
2020-05-16 21:22:42 +0700

    seeder

--- Hotel-Suggestion/database/seeds/DatabaseSeeder.php
+++ Hotel-Suggestion/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+
+        $this->call(CommentSeeder::class);
+        $this->call(HotelSeeder::class);
+        $this->call(AccountSeeder::class);
+        $this->call(HotelSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
