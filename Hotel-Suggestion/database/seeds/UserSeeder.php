88a17effe4a32b357ffb07ea34408b7776bdbb35
Ducanh0910 <61743201+Ducanh0910@users.noreply.github.com>
2020-05-16 16:57:39 +0700

    Update UserSeeder.php

--- Hotel-Suggestion/database/seeds/UserSeeder.php
+++ Hotel-Suggestion/database/seeds/UserSeeder.php
@@ -14,24 +14,20 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             ['name'=>'Tuan',
             'phone'=>'0931231242',
-            'email'=>'tuanadbac@email.com',
             'password'=>'password'
             ],
             [
             'name'=>'HieuBui',
             'phone'=>'0931231984',
-            'email'=>'Hieubui@email.com',
             'password'=>'password'
             ],
             ['name'=>'DuongPham',
             'phone'=>'09312431242',
-            'email'=>'DuongPhamc@email.com',
             'password'=>'password'
             ],
             [
             'name'=>'DucanhBui',
             'phone'=>'0931431242',
-            'email'=>'Anhducbui@email.com',
             'password'=>'password'
             ]
         ]);
