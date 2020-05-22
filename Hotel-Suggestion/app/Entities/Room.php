ab3c7a242c54e2cfe19c38a07f7c355e49cd4eea
duongpham289 <duongshin2000@gmail.com>
2020-05-22 11:30:19 +0700

    edit

--- Hotel-Suggestion/app/Entities/Room.php
+++ Hotel-Suggestion/app/Entities/Room.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    protected $table = 'room';
+
     public function hotel()
     {
         return $this->belongsTo(Hotel::class);
