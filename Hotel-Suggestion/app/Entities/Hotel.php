d3188024fe445daec188a178d114402e5fbfebb4
tuanle510 <lethientuan8668@gmail.com>
2020-05-22 09:56:09 +0700

    Update Hotel.php

--- Hotel-Suggestion/app/Entities/Hotel.php
+++ Hotel-Suggestion/app/Entities/Hotel.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Hotel extends Model
 {
     protected $table = 'hotel';
+    public function room()
+    {
+        return $this->hasMany(Room::class, 'hotel_id', 'id');
+    }
 }
