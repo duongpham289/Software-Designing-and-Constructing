e9d09008d44f89a2a7066ab1e8b0cc2344367680
Ducanh0910 <61743201+Ducanh0910@users.noreply.github.com>
2020-05-25 14:25:50 +0700

    booking

--- Hotel-Suggestion/app/Entities/Account.php
+++ Hotel-Suggestion/app/Entities/Account.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     protected $guarded = ['id'];
+    protected $table = "accounts";
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class, 'account_id','id');
+    }
 }
