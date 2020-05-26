aaac5b51318759dd60bc986e5d61277f3a90a106
duongpham289 <duongshin2000@gmail.com>
2020-05-26 21:43:20 +0700

    update

--- Hotel-Suggestion/resources/views/admin/rooms/index.blade.php
+++ Hotel-Suggestion/resources/views/admin/rooms/index.blade.php
@@ -27,23 +27,7 @@
                             <a href="/admin/rooms/create" class="btn btn-primary" >Tạo mới</a>
                         </div>
                         <div class="col-md-10">
-                            <div class="form-group">
-                                <label>Khách sạn</label>
-                                <select name="category_id" class="form-control" id="hotels"   >
-                                    {{-- <option value='1' selected>Nam</option>
-                                    <option value='3'>---|Áo khoác nam</option>
-                                    <option value='2'>Nữ</option>
-                                    <option value='4'>---|Áo khoác nữ</option> --}}
-
-                                    @forelse ($hotels as $item)
-                                        <option value="{{ $item->id }} ">{{$item->name}}</option>
-
-                                        @empty {{--Xảy ra khi rooms = NULL (Thành phần của forelse) --}}
-                                        <span>Không tìm thấy bản ghi</span>
-                                    @endforelse  {{-- Kết hợp của if và foreach --}}
-                                </select>
-                                <div id="rooms"></div>
-                            </div>
+                            @include('admin.rooms.option')
 
                         </div>
                         {{ csrf_field() }}
