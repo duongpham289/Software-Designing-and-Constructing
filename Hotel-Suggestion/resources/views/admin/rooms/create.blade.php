0560202dd5fd463d12ba41d3cbdedb674c26ec1b
duongpham289 <duongshin2000@gmail.com>
2020-05-28 23:06:57 +0700

    Revert "Merge branch 'master' of https://github.com/duongpham289/hotel-suggestion-system"
    This reverts commit 64b0bd6f905f8bd9955753a3d31ac249e94ab56a, reversing
    changes made to dd741ea0729c3537f9d0e178143d684c55b70efb.

--- Hotel-Suggestion/resources/views/admin/rooms/create.blade.php
+++ Hotel-Suggestion/resources/views/admin/rooms/create.blade.php
@@ -12,55 +12,123 @@
 		<!--/.row-->
 
 		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="page-header">Quản lý danh mục</h1>
-			</div>
-		</div>
+            <div class="col-lg-12">
+                <h1 class="page-header">Thêm Phòng</h1>
+            </div>
+        </div>
 		<!--/.row-->
 
 
 		<div class="row">
-			<div class="col-md-12">
-				<div class="panel panel-default">
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-md-5">
+            <div class="col-xs-6 col-md-12 col-lg-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Thêm Phòng</div>
+                    @if ($errors->any())
+                    {{--  <div class="alert bg-{{ $type ?? 'danger' }}" role="alert">
+                    <svg class="glyph stroked {{ $stroke }}">
+                    <use xlink:href="#stroked-{{ $stroke }}"></use>
+                    </svg> {{ $slot }}
+                    <a href="#" class="pull-right">
+                    <span class="glyphicon glyphicon-remove"></span>
+                    </a>
+                        </div>  --}}
+                        @component('admin.layouts.components.alert')
+                        @slot('type','danger')
+                        @slot('stroke','cancel')
+                        {{ $errors->first() }}
+                        @endcomponent
+                    @endif
+                    <div class="panel-body">
+                        <form action="/admin/rooms" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row" style="margin-bottom:40px">
 
-                                @if ($errors->any())
-                                @component('admin.layouts.components.alert')
-                                @slot('type', 'danger')
-                                @slot('stroke', 'cancel')
-                                {{ $errors->first() }}
-                                @endcomponent
-                                @endif
-                                {{-- <form action="/admin/categories" method="POST">
-                                    @csrf
-								<div class="form-group">
-									<label for="">Danh mục cha:</label>
-									<select class="form-control" name="parent_id" >
-										<option value="0" selected>----ROOT----</option>
-										@include('admin.rooms.option') //level có nhiệm vụ in ra --
-									</select>
-								</div>
-								<div class="form-group">
-									<label for="">Tên Danh mục</label>
-									<input type="text" class="form-control" name="name"  placeholder="Tên danh mục mới" value="Áo khoác nữ">
-								</div>
-                                <button type="submit" class="btn btn-primary">Sửa danh mục</button>
-                            </form> --}}
-							</div>
-						</div>
-					</div>
-				</div>
+                                    <div class="col-md-8">
+                                        @include('admin.rooms.option')
+                                        {{-- <div class="form-group">
+                                            <label>Mã khách sạn</label>
+                                            <input type="text" name="sku" class="form-control">
+                                        </div> --}}
+                                        <div class="form-group">
+                                            <label>Loại Phòng</label>
+                                            <input type="text" name="type" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Giá Phòng</label>
+                                            <input type="number" name="price" class="form-control">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Trạng thái phòng</label>
+                                            <select name="status" class="form-control">
+                                                <option value="1">Còn</option>
+                                                <option value="0">Không</option>
+                                            </select>
+                                        </div>
+                                        {{-- <div class="form-group">
+                                            <label>Số lượng</label>
+                                            <input type="number" class="form-control" name="quantity">
+                                        </div> --}}
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Ảnh Phòng</label>
+                                            <input id="img" type="file" name="images" class="form-control hidden"
+                                                onchange="changeImg(this)">
+                                            <img id="avatar" class="thumbnail" width="100%" height="350px" src="/assets/admin/img/import-img.png">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Chi tiết Phòng</label>
+                                            <textarea name="detail" style="width: 100%;height: 100px;"></textarea>
+                                        </div>
+                                     </div>
 
 
 
-			</div>
-			<!--/.col-->
+                        </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    {{-- <div class="form-group">
+                                        <label>Miêu tả</label>
+                                        <textarea id="editor" name="description" style="width: 100%;height: 100px;"></textarea>
+                                    </div> --}}
+                                    <button class="btn btn-success" type="submit">Thêm Phòng</button>
+                                    <button class="btn btn-danger" type="reset">Huỷ bỏ</button>
+                                </div>
+                            </div>
+                        <div class="clearfix"></div>
 
+                        </form>
+                    </div>
+                </div>
 
-		</div>
+            </div>
+        </div>
 		<!--/.row-->
 	</div>
 	<!--/.main-->
     @endsection
+    @push('adminJs')
+    <script>
+        function changeImg(input){
+               //Nếu như tồn thuộc tính file, đồng nghĩa người dùng đã chọn file mới
+               if(input.files && input.files[0]){
+                   var reader = new FileReader();
+                   //Sự kiện file đã được load vào website
+                   reader.onload = function(e){
+                       //Thay đổi đường dẫn ảnh
+                       $('#avatar').attr('src',e.target.result);
+                   }
+                   reader.readAsDataURL(input.files[0]);
+               }
+           }
+           $(document).ready(function() {
+               $('#avatar').click(function(){
+                   $('#img').click();
+               });
+           });
+
+    </script>
+@endpush
